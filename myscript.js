2cb44cbe384e34224f211270f6e90a37c10f19e1
karenwu1212 <karenwyj2012@gmail.com>
2015-03-12 20:07:00 -0600

    correct

--- myscript.js
+++ myscript.js
@@ -1,33 +1,33 @@
 //store inputs
 $('button').click(function(){
-	var inputName = $('name').value;
-	var backgroundColor = $('backgroundcolor').value;
+	var inputName = $('#name').val();
+	var backgroundColor = $('#backgroundcolor').val();
 
-	localstorage.setItem('name', inputName);
-	localstorage.setItem(inputName, backgroundColor);
+	localStorage.setItem('name', inputName);
+	localStorage.setItem(inputName, backgroundColor);
 
 })
 
 //generate GUID
-$(function GUID(){
+function GUID(){
 	function s4(){
 		return Math.floor((1 + Math.random())*0x10000).toString(16).substring(1);
 	}
-	var guid = (s4() + s4() + "-" + s4() + "-4" + s4().substr(0,3) + "-" + s4() + "-" + s4() + s4() + s4()).toLowerCase();
-})
+	return guid = (s4() + s4() + "-" + s4() + "-4" + s4().substr(0,3) + "-" + s4() + "-" + s4() + s4() + s4()).toLowerCase();
+	}
 
 //show stored inputs
 $(function showResult(){
-	var name = localstorage.getItem('name');
-	var backgroundColor = localstorage.getItem(name);
-	var guid = sessionstorage.setItem('GUID',guid);
-	var footer = $.create('footer');
-	var h = $.create('h');
-	h.textContent = "Hi" + name +", your session GUID is" + guid;
-	footer.appendChild(h);
+	var name = localStorage.getItem('name');
+	var backgroundColor = localStorage.getItem(name);
+	var guid = GUID();	
+	sessionStorage.setItem('GUID',guid);
+	var footer = $('footer');
+	var content = "Hi " + name +", your session GUID is" + guid;
+	footer.append(content);
 	if (backgroundColor) {
-		$('body').css('backgroundcolor', backgroundColor);
-};
+		$('body').css('background-color', backgroundColor);
+	};
 })
 	
 
